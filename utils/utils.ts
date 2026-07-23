05d54298294e1c1ee06c960bfc0e821a6b0e15c0
Raxon Suri <raxonsuriperez@gmail.com>
2026-07-23 02:54:56 -0400

    User's messages to global chat are being saved.

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,19 +2,34 @@
 
 import { db } from "@/app/src";
 import { messageTable } from "@/auth-schema";
-import { desc } from "drizzle-orm";
+import { asc } from "drizzle-orm";
+
+interface addExpenseInterface {
+  messageContent: string;
+  messagerId: string;
+  messagerName: string
+}
 
 export const getMessages = async() => {
   const messages = await db
     .select({
-      messageId: messageTable.messageId,
       messageContent: messageTable.messageContent,
       messagerId: messageTable.messagerId,
       messagerName: messageTable.messagerName,
     })
     .from(messageTable)
     .limit(50)
-    .orderBy(desc(messageTable.createdAt));
+    .orderBy(asc(messageTable.createdAt));
 
   return messages;
+}
+
+export const addMessage = async({messageContent, messagerId, messagerName}: addExpenseInterface) => {
+  await db
+    .insert(messageTable)
+    .values({
+      messageContent,
+      messagerId,
+      messagerName
+    })
 }
